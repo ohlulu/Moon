479a85cd45062f56042b555dde2ec8ad42848a0a
Ohlulu Hsih <z30262226@gmail.com>
2025-02-08 21:30:22 +0800

    feat: update art calculate

--- src/services/indicators/atr.py
+++ src/services/indicators/atr.py
@@ -14,6 +14,7 @@ class ATR(Indicator):
             df['close'],
             timeperiod=self.period
         )
+        df.loc[:, 'atr_pct'] = df['atr'] / df['close'] * 100
         return df
     
     def get_name(self) -> str:
